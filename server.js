56dac26de10dd30d681721cc10d7f38d46927f50
GargMukesh <30799083+GargMukesh@users.noreply.github.com>
2017-08-31 22:31:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function(req,res){
-    res.send("<h1>article one will be served</h1>");
+    res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
